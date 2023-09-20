3b85f649f025795680a3330076f9149f0c1b4942
unartcs <unartcs@gmail.com>
2023-09-20 14:35:55 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,24 @@
-import React, {  useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState, useEffect } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const form = useRef();
   const [messageSent, setMessageSent] = useState(false);
+  const SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+  const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+  const PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
   const sendEmail = (e) => {
     e.preventDefault();
     setMessageSent(true);
-    console.log(`${process.env.EMAILJS_SERVICE_ID}`, `${process.env.EMAILJS_TEMPLATE_ID}`, form.current, `${process.env.EMAILJS_PUBLIC_KEY}`)
-    emailjs.sendForm(`${process.env.EMAILJS_SERVICE_ID}`, `${process.env.EMAILJS_TEMPLATE_ID}`, form.current, `${process.env.EMAILJS_PUBLIC_KEY}`)
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY);
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
     <div
@@ -27,54 +32,58 @@ function Contact() {
         <hr className="w-[50%] absolute z-[2] opacity-50 h-px bg-red-400 border-0"></hr>
       </div>
       <div className="contact-content h-[100%] flex w-full items-center justify-center gap-[5%]">
-      {!messageSent ? 
-        <div className="form-wrapper">
-          <form
-          ref={form} onSubmit={sendEmail}
-            className="flex flex-col gap-10  justify-center [&>*]:p-2  [&>*]:border-[#e7e5e5] [&>*]:border-[0.5px] [&>*]:rounded-lg [&>*]:bg-[#191629] [&>*]:text-white"
-          >
-            <input
-              className="w-[450px] h-[35px] focus:bg-slate-600"
-              type={"text"}
-              placeholder="Name..."
-              required
-              name="name"
-              id="name"
-            ></input>
-            <input
-              className="w-[450px] h-[35px] focus:bg-slate-600"
-              type={"email"}
-              placeholder="Email..."
-              required
-              name="email"
-              id="email"
-            ></input>
-            <input
-              className="w-[450px] h-[35px] focus:bg-slate-600"
-              type={"number"}
-              placeholder="Phone Number (Optional)..."
-              name="phone"
-              id="number"
-            ></input>
-            <textarea
-              className="w-[450px] h-[200px] focus:bg-slate-600 resize-none"
-              placeholder="Message..."
-              required
-              name="message"
-              id="message"
-            ></textarea>
-            <button className="p-5 text-white hover:bg-slate-600" value='Send'>
-              Message Me!
-            </button>
-          </form>
-        </div>
-        : 
-        <div className="thanks-wrapper flex flex-col gap-10  justify-center [&>*]:text-white w-[450px] items-center text-2xl border-2 py-20">
-          <p>Message sent!</p>
-          <p>Thank you for contacting me</p>
-          <p>I will contact you shortly</p>
-        </div>
-        }
+        {!messageSent ? (
+          <div className="form-wrapper">
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="flex flex-col gap-10  justify-center [&>*]:p-2  [&>*]:border-[#e7e5e5] [&>*]:border-[0.5px] [&>*]:rounded-lg [&>*]:bg-[#191629] [&>*]:text-white"
+            >
+              <input
+                className="w-[450px] h-[35px] focus:bg-slate-600"
+                type={"text"}
+                placeholder="Name..."
+                required
+                name="name"
+                id="name"
+              ></input>
+              <input
+                className="w-[450px] h-[35px] focus:bg-slate-600"
+                type={"email"}
+                placeholder="Email..."
+                required
+                name="email"
+                id="email"
+              ></input>
+              <input
+                className="w-[450px] h-[35px] focus:bg-slate-600"
+                type={"number"}
+                placeholder="Phone Number (Optional)..."
+                name="phone"
+                id="number"
+              ></input>
+              <textarea
+                className="w-[450px] h-[200px] focus:bg-slate-600 resize-none"
+                placeholder="Message..."
+                required
+                name="message"
+                id="message"
+              ></textarea>
+              <button
+                className="p-5 text-white hover:bg-slate-600"
+                value="Send"
+              >
+                Message Me!
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="thanks-wrapper flex flex-col gap-10  justify-center [&>*]:text-white w-[450px] items-center text-2xl border-2 py-20">
+            <p>Message sent!</p>
+            <p>Thank you for contacting me</p>
+            <p>I will contact you shortly</p>
+          </div>
+        )}
         <div className="contact-info-wrapper flex flex-col gap-10">
           <a
             href="mailto:unartcs@gmail.com"
